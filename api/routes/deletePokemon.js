929db974904a097575de35151fd5225e4f78b8b1
Javier Marín <lessi.gsx@gmail.com>
2022-11-20 13:17:30 -0300

    Nueva fuente, y diseño responsivo

--- api/routes/deletePokemon.js
+++ api/routes/deletePokemon.js
@@ -6,21 +6,24 @@ var fs = require('fs');
 router.delete('/deletePokemon', function (req, res) {
   const inventoryPath = path.join(__dirname, '..', 'public', 'inv.json');
   const id = req.body.id;
+  const lastOne = req.body.lastOne;
 
   try {
     const rawFile = fs.readFileSync(inventoryPath);
     let inventory = JSON.parse(rawFile.toString());
     let newInv = {};
 
+    let keys = Object.keys(inventory);
+
     if (id != null) {
       delete inventory[id];
-
-      // ahora sortear las keys nuevamente
-      let keys = Object.keys(inventory);
 
       for (let i = 0; i < keys.length; i++) {
         newInv[i + 1] = inventory[keys[i]];
       }
+    } else if (lastOne) {
+      delete inventory[keys.length];
+      newInv = inventory;
     }
 
     fs.writeFileSync(inventoryPath, JSON.stringify(newInv, null, 2));
